84a8d5ef67a58f1315bc8e01f222979291d899d2
Diego Trabajoso <diegotrabajoso@Diegos-MacBook-Pro.local>
2024-02-17 16:56:35 -0500

    delete trash

--- main.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from app_logic import AppLogic
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = AppLogic(root)
-
-    root.mainloop()
--- prueba.py
+++
@@ -1,2 +0,0 @@
-import matplotlib
-print(matplotlib.__version__)
